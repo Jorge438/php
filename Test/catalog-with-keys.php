f653bc7bc65e353d758ad363814a145a88e6c92f
Gwendal BERRIOT <gwendal.berriot@Digital-Grenoble.local>
2022-01-24 16:59:04 +0100

    update de fin de journée

---
+++ Test/catalog-with-keys.php
@@ -0,0 +1,29 @@
+<?php
+$products = [
+
+"salas" => [
+"name" => "Marcelo Salas jersey",
+"price" => 799,
+"weight" => 110,
+"discount" => null,
+"picture_url" => "img/salas.jpg"
+],
+
+"sanchez" => [
+"name" => "Alexis Sanchez Jersey",
+"price" => 899,
+"weight" => 110,
+"discount" => null,
+"picture_url" => "img/sanchez.jpg"
+],
+
+"medel" => [
+    "name" => "Gary Medel Jersey",
+    "price" => 549,
+    "weight" => 110,
+    "discount" => null,
+    "picture_url" => "img/medel.jpg"
+    ]
+
+];
+?>
---
+++ Test/simple-catalog.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simple-catalog</title>
+</head>
+<body>
+    
+<?php
+    $products = ["iPhone", "iPad", "iMac"];
+    
+?>
+<p><?php echo implode(", ", $products); ?></p>
+<p><?php echo ?></p>
+<p><?php echo ?></p>
+
+</body>
+</html>
