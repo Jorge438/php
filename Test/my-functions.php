1e2ee8263eee86a602ddf5f9fb7972adb6c545b1
Gwendal BERRIOT <gwendal.berriot@Digital-Grenoble.local>
2022-01-26 14:34:19 +0100

    ajout de la fonction calculant le prix hors taxe à partir d'un prix toutes taxes comprises

--- Test/my-functions.php
+++ Test/my-functions.php
@@ -3,4 +3,9 @@
         $price_in_cents = $price_in_cents/100;
         return $price_in_cents;
     }
+
+    function price_excluding_vat($price_incl_tax) {
+        $price_excl_tax = (100*$price_incl_tax)/(100+10);
+        return $price_excl_tax;
+    }
 ?>
